c63f0b1166a36f2f0b74a1747a690598b57760f8
vineetsarpal <vineetsarpal02@gmail.com>
2026-03-11 17:27:53 -0400

    Implement cutting progress

---
+++ Assets/Scripts/ProgressBarUI.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarUI : MonoBehaviour
+{
+    [SerializeField] private CuttingCounter cuttingCounter;
+    [SerializeField] private Image barImage;
+
+    private void Start()
+    {
+        cuttingCounter.OnProgressChanged += CuttingCounter_onProgressChanged;
+        barImage.fillAmount = 0f;
+        Hide();
+    }
+
+    private void CuttingCounter_onProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
+    {
+        barImage.fillAmount = e.progressNormalized;
+        if (e.progressNormalized == 0f || e.progressNormalized == 1f)
+        {
+            Hide();
+        } else
+        {
+            Show();
+        }
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
