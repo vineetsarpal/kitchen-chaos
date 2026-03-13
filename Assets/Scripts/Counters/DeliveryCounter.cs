4731448870c661d93e14b2b0184e502370a9e67f
vineetsarpal <vineetsarpal02@gmail.com>
2026-03-12 23:01:59 -0400

    Create delivery counter shader

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                // only accepts plate
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
